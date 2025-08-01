5fb39600e8321777997229f822d771d043e1f8a5
Bruno Loredo <brunoloredo91@gmail.com>
2025-08-01 15:50:58 -0300

    Feat: Adciona botao fechar

--- Scene/popups/tela_opcoes.gd
+++
@@ -1,9 +0,0 @@
-extends MarginContainer  # ou qualquer que seja o nó raiz
-
-@onready var botao_fechar: Button = $VBoxContainer/HBoxContainer/BotaoFechar
-
-func _ready():
-	botao_fechar.pressed.connect(fechar_tela)
-
-func fechar_tela():
-	queue_free()
